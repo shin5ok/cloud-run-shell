2037cd76f2404060426563927620365401f4dd3d
shin5ok <shin5ok@55mp.com>
2023-06-25 00:21:51 +0900

    make Response class response when raising error due to auth failure

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ import subprocess
 from subprocess import PIPE
 
 app = FastAPI()
-
 secret = os.environ.get("SECRET")
 port = os.environ.get("PORT", "8080")
-SECRET_HEADER_KEY = "X-MyGCP-Secret"
+secret_header_key = "X-MyGCP-Secret"
 
 class Command(BaseModel):
-    command: str
     secret: Union[str, None] = None
+    command: str
 
 class Output(BaseModel):
-    stdout_output: List[str]
-    stderr_output: List[str]
-    return_code: int
+    stdout_output: List[str] = []
+    stderr_output: List[str] = []
+    message: str = ""
+    return_code: int = 0
 
 @app.post("/shellcommand")
 def cmd(command: Command, request: Request, response: Response, x_mygcp_secret = Header(default=None)):
@@ -30,13 +30,20 @@ def cmd(command: Command, request: Request, response: Response, x_mygcp_secret =
 
     if secret and command.secret != secret and secret != secret_in_request:
         response.status_code = 503
-        return {"output":f"Invalid auth. Set {SECRET_HEADER_KEY} with valid secret value."}
+        return Output(
+                message=f"Invalid auth. Set {secret_header_key} with valid secret value.",
+                return_code=1,
+            )
 
     proc = subprocess.run(cmd_str, shell=True, stdout=PIPE, stderr=PIPE, text=True)
     stdout_output = proc.stdout.split("\n")
     stderr_output = proc.stderr.split("\n")
 
-    return Output(stderr_output=stderr_output, stdout_output=stdout_output, return_code=proc.returncode)
+    return Output(
+            stderr_output=stderr_output,
+            stdout_output=stdout_output,
+            return_code=proc.returncode,
+        )
 
 if __name__ == "__main__":
     options = {
