07f38061c397a2282e3d1cd830390146a379894e
shin5ok <shin5ok@55mp.com>
2023-08-10 11:51:20 +0900

    specify resouces to be used in Cloud Run instance

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@
 .PHONY: client
 client:
 	( cd client ; go build -o client . )
+
+.PHONY: deploy
+deploy:
+	bash ./deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,9 @@
-gcloud run deploy shellcommand --source=. --set-env-vars=SECRET=gcp --region=asia-northeast1 --execution-environment=gen2 --timeout=3600 $@
+gcloud run deploy shellcommand --source=. \
+    --set-env-vars=SECRET=gcp \
+    --region=asia-northeast1 \
+    --execution-environment=gen2 \
+    --memory=1Gi \
+    --cpu=1 \
+    --max-instances=1 \
+    --timeout=3600 \
+    $@
