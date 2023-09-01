ba47f32b21d45c5268c6f4a926ffb092f70a338d
shin5ok <shin5ok@55mp.com>
2023-09-01 16:37:16 +0900

    give capability to have option to choose generations

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 SECRET=${SECRET:-gcp}
+GEN=${GEN:-gen2}
+SERVICE_NAME=${SERVICE_NAME:-shellcommand}
 
-gcloud run deploy shellcommand --source=. \
+gcloud run deploy ${SERVICE_NAME} --source=. \
     --set-env-vars=SECRET=${SECRET} \
     --region=asia-northeast1 \
-    --execution-environment=gen2 \
+    --execution-environment=${GEN} \
     --memory=1Gi \
     --cpu=1 \
     --max-instances=1 \
