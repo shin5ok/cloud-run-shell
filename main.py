70609ab381dd3075b363080a92ca9b33a4f1ceb3
shin5ok <shin5ok@55mp.com>
2023-09-13 13:14:18 +0900

    get metadata from internal api server

--- main.py
+++ main.py
@@ -6,11 +6,25 @@ from typing import Union, List
 import uvicorn
 import subprocess
 from subprocess import PIPE
+import requests
 
 app = FastAPI()
 secret = os.environ.get("SECRET", "gcp")
 port = os.environ.get("PORT", "8080")
 secret_header_key = "X-MyGCP-Secret"
+my_id: str = ""
+
+def _get_my_id() -> str:
+    global my_id
+    if my_id:
+        return my_id
+    try:
+        headers = {'Metadata-Flavor':'Google'}
+        response = requests.get("http://metadata.google.internal/computeMetadata/v1/instance/id", headers=headers, timeout=0.1)
+        my_id = response.content
+        return my_id
+    except:
+        return "__no_name__"
 
 class Command(BaseModel):
     secret: Union[str, None] = None
@@ -21,6 +35,7 @@ class Output(BaseModel):
     stderr_output: List[str] = []
     message: str = ""
     return_code: int = 0
+    metadata: List[dict[str, str]]
 
 @app.post("/shellcommand")
 def cmd(command: Command, request: Request, response: Response, x_mygcp_secret = Header(default=None)):
@@ -35,6 +50,7 @@ def cmd(command: Command, request: Request, response: Response, x_mygcp_secret =
             stderr_output=stderr_output,
             stdout_output=stdout_output,
             return_code=proc.returncode,
+            metadata=[dict(instance_id=_get_my_id())],
         )
 
 @app.middleware("http")
