3d77dbbaf0ae4ab0dc488dc0d213b870014215c3
shin5ok <shin5ok@55mp.com>
2023-09-13 13:37:29 +0900

    make feature to get CPU info

--- bench/main.go
+++ bench/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
-
-	"github.com/mohae/joefriday/cpu/cpuinfo"
+	"os"
+	"strings"
 )
+
+func getCPUInfo() []string {
+	f, _ := os.Open("/proc/cpuinfo")
+	s := bufio.NewScanner(f)
+	info := make([]string, 128)
+	for s.Scan() {
+		info = append(info, s.Text())
+	}
+	return info
+}
 
 func fibo(n int) int {
 	if n < 2 {
@@ -14,16 +25,22 @@ func fibo(n int) int {
 	return fibo(n-2) + fibo(n-1)
 }
 
+func init() {
+	info := getCPUInfo()
+	fmt.Print(strings.Join(info, "\n"))
+	os.Exit(0)
+}
+
 func main() {
 	t := flag.Bool("cpu", false, "")
 	n := flag.Int("n", 1, "")
+	flag.Parse()
 
 	if *t {
-		info, _ := cpuinfo.Get()
-		fmt.Printf("%+#v", info)
+		info := getCPUInfo()
+		fmt.Print(strings.Join(info, "\n"))
+		os.Exit(0)
 	}
-
-	flag.Parse()
 
 	fmt.Println(fibo(*n))
 }
